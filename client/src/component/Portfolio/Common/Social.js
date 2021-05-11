eb8f79cbb0bf963b3f297d2ed9a5a606a2161552
Dilkash Shaikh Shahagir Mahajan <61229215+Dilkash-Shaikh-Mahajan@users.noreply.github.com>
2021-05-12 01:51:46 +0530

    Changes in social link

--- client/src/component/Portfolio/Common/Social.js
+++ client/src/component/Portfolio/Common/Social.js
@@ -5,20 +5,22 @@ const Social = () => {
 		<div>
 			<div className='social-link my-3'>
 				Find Me :
-				<a href='https://www.facebook.com/dilkash.mahajan.7'>
-					<i className='fab fa-facebook-f'></i>
-				</a>
-				<a href='https://github.com/Dilkash-Web-Developer'>
-					<i className='fab fa-github'></i>
-				</a>
-				<a href='https://twitter.com/Dilkashmahajan3'>
-					<i className='fab fa-twitter'></i>
-				</a>
-				<a href='https://www.linkedin.com/in/dilkash-shaikh-mahajan-322b2b147/'>
+		<a href='https://www.linkedin.com/in/dilkash-shaikh-mahajan/'>
 					<i className='fab fa-linkedin-in'></i>
 				</a>
-				<a href='https://www.instagram.com/mr.dilkash_shaikh'>
+				
+				<a href='https://github.com/Dilkash-Shaikh-Mahajan'>
+					<i className='fab fa-github'></i>
+				</a>
+				<a href='https://twitter.com/Dilkashmahajan7'>
+					<i className='fab fa-twitter'></i>
+				</a>
+				
+				<a href='https://www.instagram.com/dilkash_shaikh_mahajan7/'>
 					<i className='fab fa-instagram'></i>
+				</a>
+		<a href='https://www.facebook.com/dilkash.shaikh.mahajan.7/'>
+					<i className='fab fa-facebook-f'></i>
 				</a>
 			</div>
 		</div>
